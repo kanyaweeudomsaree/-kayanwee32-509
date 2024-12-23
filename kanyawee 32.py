93e1ec905645110633b6e23b5abd0b07b03debb3
kanyaweeudomsaree <B.kanyawee14@gmail.com>
2024-12-23 14:34:02 +0700

    Add files via upload

---
+++ kanyawee 32.py
@@ -0,0 +1,11 @@
+width = float(input())
+length = float(input())
+depth = float(input())
+
+volum = width * length * depth
+
+x = 15/ 60
+
+total = volum*x
+
+print(f'ต้องใช้เวลาในการเติมน้ำ{total : 2f}นาที')
